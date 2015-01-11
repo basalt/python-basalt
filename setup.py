0a5ebb244644804cc29b586a9e5b7a2471ae7e00
Jochen Breuer <breuer@dajool.com>
2015-01-11 19:34:05 +0100

    Added task helper files and setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basalt',
+    version='0.1',
+    packages=find_packages("."),
+    include_package_data=True,
+    url='https://github.com/basalt/python-basalt',
+    license='MIT',
+    author='Jochen Breuer',
+    author_email='brejoc@gmail.com',
+    install_requires=[
+        'invoke',
+        'PyYAML',
+        'Jinja2',
+        'sh',
+    ],
+    description='',
+    platforms='any',
+)
