9ca67c064b6577d85c28c22653cc16c3ecd9ea1a
Jochen Breuer <breuer@dajool.com>
2017-04-13 11:59:13 +0200

    Version bump to 0.2
    Bumping version to 0.2
    
    Changelog:
        * Added VCS module to gather information about branches and commit
          revisions. Only available for git at the moment.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='basalt-tasks',
-    version='0.1.1',
+    version='0.2',
     packages=find_packages("."),
     include_package_data=True,
     url='https://github.com/basalt/python-basalt',
